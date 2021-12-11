b049b6aaedb1f9a091ef3917eac5a7d306662c87
Jayant Kaushik <jayantdps@gmail.com>
2021-12-11 20:34:09 +0530

    TextUTil application + darkmode(commented out)
    TextUtil application + dark mode(commented out) but code exists for future reference

---
+++ src/components/About.js
@@ -0,0 +1,89 @@
+import React,{useState} from 'react'
+
+export default function About() {
+
+// let myStyle = {
+//     color: 'white',
+//     background: 'black'
+// }
+
+const [myStyle, setMyStyle] = useState({
+    color: 'black',
+        background: 'white'
+})
+const [btnText, setBtnText] = useState("Enable Dark Mode")
+
+const toggleStyle = () =>{
+    if (myStyle.color === 'black') {
+        setMyStyle(
+            {
+                color: 'white',
+                background: 'black',
+                border : '1px solid white' 
+            }
+        )
+        setBtnText("Enable Light Mode")
+    }
+    else{
+        setMyStyle(
+            {
+                color: 'black',
+                background: 'white' 
+            }
+        )
+        setBtnText("Enable Dark Mode")
+    }
+}
+
+    return (
+        <div className="container" style="myStyle">
+            <h1>About Us</h1>
+        <div id="accordion" style="myStyle">
+  <div ClassName="card">
+    <div ClassName="card-header" id="headingOne">
+      <h5 ClassName="mb-0">
+        <button ClassName="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Collapsible Group Item #1
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" ClassName="collapse show" aria-labelledby="headingOne" data-parent="#accordion" style="myStyle">
+      <div ClassName="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div ClassName="card">
+    <div ClassName="card-header" id="headingTwo">
+      <h5 ClassName="mb-0">
+        <button ClassName="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Collapsible Group Item #2
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" ClassName="collapse" aria-labelledby="headingTwo" data-parent="#accordion" style="myStyle">
+      <div ClassName="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div ClassName="card">
+    <div ClassName="card-header" id="headingThree">
+      <h5 ClassName="mb-0">
+        <button ClassName="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Collapsible Group Item #3
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" ClassName="collapse" aria-labelledby="headingThree" data-parent="#accordion" style="myStyle">
+      <div ClassName="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
+<button onclick={toggleStyle} type="button" className="btn btn-primary">{btnText}</button>
+        </div>
+    )
+}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,17 +7,46 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = () =>{
+        // console.log("uppercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleClearClick = () =>{
+        // console.log("uppercase was clicked" + text);
+        let newText = '';
+        setText(newText);
+    }
+    const handleDupClick = () =>{
+        // console.log("uppercase was clicked" + text);
+        let newText = text.repeat(2);
+        setText(newText);
+    }
+    const handleCopy = () => {
+        console.log("I am a copy")
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
 
     const handleOnChange = (event) =>{
         // console.log("on change!!");
         setText(event.target.value);
     }
+    
 
 
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
     // setText("new text");
     return (
-        <div>
+        <>
+        <div className="container">
             <h1>{props.heading}</h1>
             <div className="mb-3">
             <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
@@ -25,6 +54,30 @@ export default function TextForm(props) {
             <button className="btn btn-primary" onClick={handleUpClick}>
              Convert to UPPERCASE
             </button>
+            <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+             Convert to Lowercase
+            </button>
+            <button className="btn btn-primary mx-2" onClick={handleDupClick}>
+             Duplicate text
+            </button>
+            <button className="btn btn-primary mx-2" onClick={handleCopy}>
+             Copy text
+            </button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>
+             Remove extra space
+            </button>
+            <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+             Clear text
+            </button>
+         
         </div>
+        <div className="container my-3">
+            <h2> Your text summary</h2>
+            <p> {text.split(" ").length} words and {text.length} characters</p>
+            <p> {0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
