6d4ec3aac408af12e7f5c26c106bd81f53f45ee1
Jayant Kaushik <jayantdps@gmail.com>
2021-12-12 09:45:04 +0530

    adding my name
    added my name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TextForm from './components/TextForm';
 function App() {
   return (
  <>
- <Navbar title= "TextUtils" aboutText="About TextUtils"/>
+ <Navbar title= "TextUtils by Jayant" aboutText="About TextUtils"/>
  <div className="container my-3">
  <TextForm heading = "Enter the text to analyze below"/>
  {/* <About /> */}
